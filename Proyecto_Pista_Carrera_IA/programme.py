88ac14df1df210c537f3b7725a87bba2133bdbb7
GabrielChaconA <citroustax8849@gmail.com>
2025-11-16 19:56:09 -0600

    alteraciones

--- Proyecto_Pista_Carrera_IA/programme.py
+++ Proyecto_Pista_Carrera_IA/programme.py
@@ -2,42 +2,93 @@
 from ursina import *
 from pathlib import Path
 
-class Programme: 
-        """ AQUI HAY QUE HACER TODO EL CODE """
-        app = Ursina()
+app = Ursina()
 
-        #Carpeta 
-        application.asset_folder = Path(r"C:\Users\gaboc\OneDrive\Documentos\TECMORELIA\Quinto semestre\Proyecto de graficación\Proyecto_Pista_Carrera_IA\Modelos 3d")
 
-        #Pista 2d
-        pista = Entity(
-            model='FullTrack.obj',
-            texture = 'Main.png',
-            color=color.light_gray,
-            scale=10,           
-            position=(0.1, 0.5, 0),   
-            rotation=(0, 0, 0),
-            double_sided=True,
-            shadows = True
-        )
+# Ruta de assets relativa al .py
+base_path = Path(__file__).parent
+application.asset_folder = base_path / "Modelos 3d"
 
-        #Modelo del carro 
-        modelo = Entity(
-            model='DeLorean.obj',
-            color=color.light_gray,
-            scale=0.02,           
-            position=(0.1, 0.5, 0),   
-            rotation=(0, 0, 0),
-            double_sided=True
-        )
 
-        # === CÁMARA LIBRE ===
-# Mueve con W/A/S/D/Q/E y gira con clic derecho + ratón
-        cam = EditorCamera(rotation=(35, 0, 0), position=(0, 10, -20))
+# === PISTA ===
+pista = Entity(
+    model='FullTrack.obj',
+    texture='Main.png',
+    color=color.light_gray,
+    scale=15,
+    position=(0, 0, 0),
+    rotation=(0, 0, 0),
+    double_sided=True,
+    shadows=True,
+    collider='mesh'        
+)
+
+
+# === CARRO ===
+DeloRean = Entity(
+    model='DeLorean.obj',
+    color=color.light_gray,
+    scale=(-0.02, 0.02, 0.02 ),
+    position=(4.5, 2.90, 0),
+    rotation=(0, 0, 0),
+    double_sided=True
+)
+
+# Esfera a cierta distancia delante del carro (hija del pivot)
+lead_dist = 6
+esfera = Entity(parent=DeloRean, model='sphere', color=color.red, scale=.3, position=(0,0,6))
+
+# Cámara
+EditorCamera(pivot=DeloRean, position=(4.5, 2.90, 0), rotation=(25,0,0))
+
+vel = 0.25
+def update():
+    EPS = 0.02        # tolerancia para considerar y≈0
+    ADELANTE = 0.0    # pon 2.0 si quieres medir un poco al frente del carro
+    i =0
+
+
+    # Dirección HACIA la esfera usando POSICIÓN MUNDIAL
+    direccion = (esfera.world_position - DeloRean.world_position)
+    
+    # Hacer que el carro mire a la esfera (solo yaw)
+    DeloRean.look_at(esfera)
+
+    # MOVER EL CARRO
+    if held_keys['w']:
+        DeloRean.position += direccion * vel
+    if held_keys['s']:
+        DeloRean.position -= direccion * vel
+    if held_keys['a']: 
+        DeloRean.rotation_y -= 1
+    if held_keys['d']:
+        DeloRean.rotation_y += 1
+
+    origin = DeloRean.world_position + DeloRean.forward * ADELANTE + Vec3(0, 1, 0)
+
+    hit = raycast(
+        origin=origin,
+        direction=Vec3(0, -1, 0),
+        distance=100,
+        ignore=(DeloRean,),
+        traverse_target=scene
+    )
+
+    if hit.hit and hit.entity == pista:
+
+        DeloRean.y = hit.world_point.y
+    print(DeloRean.position)
+        
+        
+
+
+           
+    
      
+    
 
-        # aqui se realizaran los updates del movimiento del carro
-        def update():
-            pass
+            
 
-        app.run()
+app.run()
+
+        
